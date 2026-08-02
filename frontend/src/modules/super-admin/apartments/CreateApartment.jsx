7da0a77912151f441511d77345baa9175386de2b
luckytsori8@gmail.com <luckytsori8@gmail.com>
2026-08-02 22:12:27 +0300

    feat(apartments): implement complete apartment management module

---
+++ frontend/src/modules/super-admin/apartments/CreateApartment.jsx
@@ -0,0 +1,9 @@
+const CreateApartment = () =>{
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default CreateApartment
---
+++ frontend/src/modules/super-admin/apartments/DeleteApartmentModal.jsx
@@ -0,0 +1,9 @@
+const DeleteApartmentModal = () =>{
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default DeleteApartmentModal
